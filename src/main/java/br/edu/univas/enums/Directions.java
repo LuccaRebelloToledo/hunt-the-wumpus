0ca5c2153b29becf305e185c0940d98a0d286c48
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-06-15 15:25:40 -0300

    chore: unused code

--- src/main/java/br/edu/univas/enums/Directions.java
+++ src/main/java/br/edu/univas/enums/Directions.java
@@ -1,18 +1,8 @@
 package br.edu.univas.enums;
 
 public enum Directions {
-    NORTH("Norte"),
-    SOUTH("Sul"),
-    EAST("Leste"),
-    WEST("Oeste");
-
-    private final String direction;
-
-    Directions(String direction) {
-        this.direction = direction;
-    }
-
-    public String getDirection() {
-        return this.direction;
-    }
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
 }
